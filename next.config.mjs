45e4473562f103527ad5b44344ebb896a563be6a
nexusboyko <alexboyko@outlook.com>
2024-10-13 10:07:07 -0700

    Add comments for local env setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  env: {
+    // to run it locally, you need:
+    // OPENAI_API_KEY: <your openai api key>
+    // optional (not used yet)
+    // (user auth)
+    // GOOGLE_CLIENT_ID: ...
+    // GOOGLE_CLIENT_SECRET: ...
+    // AUTH_SECRET: ...
+    // (monogodb)
+    // DATABASE_URL: ...
+  },
   images: {
     domains: ["lh3.googleusercontent.com"],
   },
